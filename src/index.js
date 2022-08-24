de4a26185dc6faad2ad127bf6199947353a21d77
Johnny Hildebrand <74218092+JohnnyHildebrand@users.noreply.github.com>
2022-08-24 16:51:38 -0500

    Added Click Handlers

--- src/index.js
+++ src/index.js
@@ -3,18 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
+       render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" 
+        onClick={() => this.props.onClick()}>
+          {this.props.value}
         </button>
       );
     }
+    handelClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares : squares});
+      }
   }
+ 
   
   class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            squares: Array(9).fill(null),
+        }
+    }
     renderSquare(i) {
-      return <Square />;
+      return <Square value={this.state.squares(i)}
+      onClick={()=> this.handelClick(i)} />;
     }
   
     render() {
